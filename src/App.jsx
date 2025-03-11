2e7e8e28292ebde8ca4f1b0649e9fbfeceeb742c
Cauã Vieira <cauav_almeida@hotmail.com>
2025-03-11 20:14:40 -0300

    feat: post

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Post } from "./components/Post";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
 import './app.css';
 import styles from './App.module.css';
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,9 +2,36 @@ import styles from './Post.module.css';
 
 export function Post (props) {
     return (
-        <div>
-            <strong>{props.author}</strong>
-            <p>{props.content}</p>
-        </div>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/dcauav.png"/>
+                    <div className={styles.authorInfo}>
+                        <strong>Cauã Vieira</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time title="11 de Março às 18:46" dateTime="2025-03-11 18:46:00">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>{' '}
+                    <a href="#">#nlw</a>{' '}
+                    <a href="#">#rocketseat</a>
+                </p>
+            </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe o seu feedback</strong>
+                <textarea placeholder="Deixe um comentário"/>
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
+        </article>
     )
 }
