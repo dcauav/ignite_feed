8a636310bd40349bc8d8f225e46348a7f5fe40ef
Cauã Vieira <cauav_almeida@hotmail.com>
2025-03-16 09:32:58 -0300

    feat: useState para comentários

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,56 @@ import { Post } from "./components/Post";
 import './app.css';
 import styles from './App.module.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera!' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio.' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-03-15 13:31:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera!' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifólio.' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-03-15 13:31:00')
+  }
+]
+
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <div className={styles.wrapper}>
-        <Sidebar/>
+        <Sidebar />
         <main>
-          <Post 
-            author="Cauã Vieira" 
-            content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae maiores perspiciatis non, quasi exercitationem placeat laudantium id asperiores fuga iure. Nisi debitis placeat sapiente ad architecto repellat ipsa accusamus molestiae!"
-          />
-          <Post 
-            author="Diego Fernandes" 
-            content="Um novo post muito legal!"
-          />
+          {
+            posts.map((post) => { 
+              return ( 
+                <Post 
+                  id={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              ) 
+            })
+          }
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,44 +1,74 @@
-import styles from './Post.module.css';
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
-export function Post (props) {
+import styles from './Post.module.css';
+import { useState } from 'react';
+
+export function Post({ author, content, publishedAt }) {
+    const [comments, setComments] = useState([
+        1,
+        2
+    ]);
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    });
+
+    function handleCreateNewComment(event) {
+        event.preventDefault();
+        setComments([...comments, comments.length + 1]);
+    }
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/dcauav.png"/>
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Cauã Vieira</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
-                <time title="11 de Março às 18:46" dateTime="2025-03-11 18:46:00">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p>👉{' '}<a href="#">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="#">#novoprojeto</a>{' '}
-                    <a href="#">#nlw</a>{' '}
-                    <a href="#">#rocketseat</a>
-                </p>
+                {
+                    content.map((line) => {
+                        switch (line.type) {
+                            case 'paragraph':
+                                return <p>{line.content}</p>
+                            case 'link': 
+                                return <p><a href="#">{line.content}</a></p>
+                            default:
+                                break;
+                        }
+                    })
+                }
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe o seu feedback</strong>
-                <textarea placeholder="Deixe um comentário"/>
+                <textarea placeholder="Deixe um comentário" />
                 <footer>
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
 
             <div className={styles.commentList}>
-                <Comment></Comment>
-                <Comment></Comment>
-                <Comment></Comment>
+                {
+                    comments.map((comment) => {
+                        return <Comment />
+                    })
+                }
             </div>
         </article>
     )
