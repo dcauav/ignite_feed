4055722e8d363d0ddf555da4581c224682ef9681
Cauã Vieira <cauav_almeida@hotmail.com>
2025-03-18 21:38:15 -0300

    feat: refatoramento para typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import { Post } from "./components/Post";
+import { Post, PostType } from "./components/Post";
 
 import './app.css';
 import styles from './App.module.css';
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -48,9 +48,7 @@ function App() {
               return ( 
                 <Post 
                   key={post.id}
-                  author={post.author}
-                  content={post.content}
-                  publishedAt={post.publishedAt}
+                  post={post}
                 />
               ) 
             })
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,31 +1,53 @@
 import { format, formatDistanceToNow } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
+import { ptBR } from 'date-fns/locale/pt-BR';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 
 import styles from './Post.module.css';
-import { useState } from 'react';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 
-export function Post({ author, content, publishedAt }) {
+interface Author {
+    name: string;
+    role: string;
+    avatarUrl: string;
+}
+
+interface Content {
+    type: 'paragraph' | 'link';
+    content: string;
+}
+
+export interface PostType {
+    id: number;
+    author: Author;
+    content: Content[];
+    publishedAt: Date;
+}
+
+interface PostProps {
+    post: PostType;
+}
+
+export function Post({ post }: PostProps) {
     const [comments, setComments] = useState([
         'Post muito bacana!',
     ]);
     const [newCommentText, setNewCommentText] = useState('');
 
 
-    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    const publishedDateFormatted = format(post.publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBR
     });
 
-    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    const publishedDateRelativeToNow = formatDistanceToNow(post.publishedAt, {
         locale: ptBR,
         addSuffix: true
     });
 
     const isNewCommentEmpty = newCommentText.trim().length === 0;
     
-    function handleCreateNewComment(event) {
+    function handleCreateNewComment(event: FormEvent) {
         event.preventDefault();
 
         if (!isNewCommentEmpty) {
@@ -34,16 +56,16 @@ export function Post({ author, content, publishedAt }) {
         }
     }
 
-    function handleNewCommentChange(event) {
+    function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
     }
 
-    function handleNewCommentInvalid(event) {
+    function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
         event.target.setCustomValidity('Esse campo é obrigatório!');
     }
 
-    function deleteComment(commentToDelete) {
+    function deleteComment(commentToDelete: string) {
         setComments(state => state.filter((comment) => comment !== commentToDelete));
     }
     
@@ -51,18 +73,18 @@ export function Post({ author, content, publishedAt }) {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src={author.avatarUrl} />
+                    <Avatar src={post.author.avatarUrl} hasBorder={true} />
                     <div className={styles.authorInfo}>
-                        <strong>{author.name}</strong>
-                        <span>{author.role}</span>
+                        <strong>{post.author.name}</strong>
+                        <span>{post.author.role}</span>
                     </div>
                 </div>
-                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+                <time title={publishedDateFormatted} dateTime={post.publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
             </header>
 
             <div className={styles.content}>
                 {
-                    content.map((line) => {
+                    post.content.map((line) => {
                         switch (line.type) {
                             case 'paragraph':
                                 return <p key={line.content}>{line.content}</p>
